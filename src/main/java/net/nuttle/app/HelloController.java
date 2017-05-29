7e6e581a0443c8fdc773bb71e1ca861cdefa352c
d_nuttle@hotmail.com <d_nuttle@hotmail.com>
2017-05-29 14:18:10 -0500

    First commit

---
+++ src/main/java/net/nuttle/app/HelloController.java
@@ -0,0 +1,18 @@
+package net.nuttle.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
+  
+  @RequestMapping("/hello")
+  public String index() {
+    LOG.debug("index");
+    return "Greetings from Spring Boot";
+  }
+}
