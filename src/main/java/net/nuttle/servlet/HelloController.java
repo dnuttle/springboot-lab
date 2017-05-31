2369d95bec3d19c82bc6acee4f37caf2a9c90771
d_nuttle@hotmail.com <d_nuttle@hotmail.com>
2017-05-31 07:15:31 -0500

    Experiment with modle/view, Thymeleaf

--- src/main/java/net/nuttle/servlet/HelloController.java
+++ src/main/java/net/nuttle/servlet/HelloController.java
@@ -5,17 +5,18 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import net.nuttle.bean.TestBean;
 
 //RestController is annotated with both Controller and ResponseBody
-@RestController
+@Controller
 public class HelloController {
 
   private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
@@ -24,6 +25,7 @@ public class HelloController {
   TestBean bean;
   
   @RequestMapping(value="/hello", method=RequestMethod.GET)
+  @ResponseBody
   public String hello(@RequestParam Map<String, String> allRequestParams, ModelMap map) {
     LOG.debug("hello");
     allRequestParams.forEach((key, val) -> {
@@ -37,12 +39,25 @@ public class HelloController {
   /*
    * @ResponseBody isn't required when class has @RestController.
    * Its purpose is to indicate that response should be written directly into http response body,
-   * not placed into a model or interpreted as a view name.  Still not entirely clear on 
-   * what is meant by that.
+   * not placed into a model or interpreted as a view name.
+   * To return a model and view when RestController is in place, and therefore the method has
+   * an implicate ResponseBody, return ModelAndView instead.
+   * Otheriwse use @Controller on the class and @ResponseBody only on methods where that is needed.
    */
   @RequestMapping(value="/bean", method=RequestMethod.GET)
   @ResponseBody
   public TestBean bean(@RequestParam Map<String, String> allRequestParams, ModelMap map) {
     return bean;
+  }
+  
+  @RequestMapping(value="/template", method=RequestMethod.GET)
+  @ResponseBody
+  public ModelAndView template() {
+    ModelAndView mv = new ModelAndView();
+    //Template name defaults to the mapping, but can be changed
+    mv.setViewName("login");
+    mv.addObject("name", "A name value");
+    mv.addObject("login", "A login name");
+    return mv;
   }
 }
